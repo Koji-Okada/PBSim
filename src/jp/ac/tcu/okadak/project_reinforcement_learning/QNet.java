8139e7fe7999994aaaa4a0c62cc8595feb7c926b
K. Okada <koji.okada.prog@gmail.com>
2021-08-19 00:59:15 +0900

    試行錯誤

--- src/jp/ac/tcu/okadak/project_reinforcement_learning/QNet.java
+++ src/jp/ac/tcu/okadak/project_reinforcement_learning/QNet.java
@@ -52,6 +52,9 @@ public class QNet {
 		System.out.println("Start QNet ...");
 
 		QNet qNetObj = new QNet();
+		
+		qNetObj.test();
+		
 //		int[] inNodeLevels = { 10, 4, 4, 4, 4, 4, 4, 4, 4 };
 		int[] inNodeLevels = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
@@ -61,9 +64,34 @@ public class QNet {
 
 		System.out.println("... Fin.");
 
+
+		
+		
 		return;
 	}
 
+	/**
+	 * 
+	 */
+	void test() {
+		
+		double[][] a = new double[10][1];
+		for (int i = 1; i < 10; i++) {
+			a[i][0] = (double)i * -50e0D;
+		}
+		
+		INDArray b = Nd4j.create(a);		
+		System.out.println(b);
+		
+		INDArray c = transOutData(b);
+		System.out.println(c);
+
+		INDArray d = invOutData(c);
+		System.out.println(d);
+		
+	}
+	
+	
 	/**
 	 * Q-Netを生成する.
 	 *
@@ -135,7 +163,8 @@ public class QNet {
 		// 入力側データを設定する
 		float[][] data = new float[rMax][cMax];
 		for (int i = 0; i < rMax; i++) {
-			for (int j = 0; j < cMax; j++) {
+			data[i][0] = sampleX() * 0.7e0F;
+			for (int j = 1; j < cMax; j++) {
 				data[i][j] = sampleX();
 			}
 		}
@@ -149,8 +178,8 @@ public class QNet {
 //		INDArray outData = Nd4j.create(res);
 
 		// テスト用
-		int num = 12;
-		int num2 = 3;
+		int num = 10;
+		int num2 = 2;
 		float[][] test = new float[num][9];
 		for (int i = 0; i < num; i++) {
 			for (int j = 0; j < 9; j++) {
@@ -160,7 +189,9 @@ public class QNet {
 				}
 			}
 			if (i < num2) {
-				res[i][0] = -500.0e0F;
+				test[i][0] = 1.0e0F;
+				data[i][0] = 1.0e0F;
+				res[i][0] = -50.0e0F;
 			}
 		}
 		INDArray outData = Nd4j.create(res);
@@ -250,8 +281,10 @@ public class QNet {
 
 		INDArray transIn = transInData(in);
 		INDArray transOut = transOutData(out);
-		double score = planeUpdate(transIn, transOut);
+//		double score = planeUpdate(transIn, transOut);
+		double score = planeUpdate(transIn, out);
 
+		
 		return score;
 	}
 
@@ -269,7 +302,7 @@ public class QNet {
 
 		double score = 1.0e3D; // 大きめの値から
 		double preScore = 1.2e3D;
-		while ((score > 1.0e-4D) && (preScore - score > 1.0e-6D)) {
+		while ((score > 1.0e-5D) && (preScore - score > 1.0e-6D)) {
 			for (int i = 0; i < nEpochs; i++) {
 				data.shuffle();
 				qNet.fit(data); // 学習する
@@ -292,6 +325,9 @@ public class QNet {
 
 		INDArray transIn = transInData(in);
 		INDArray out = qNet.output(transIn);
+		INDArray inv = invOutData(out);
+//		return inv;
+		
 		return out;
 	}
 
@@ -342,21 +378,58 @@ public class QNet {
 
 	}
 
+	
+	private double conv = 0.0e0D;
 	/*
 	 * 
 	 */
 	private INDArray transOutData(INDArray out) {
 
 		int nSamples = (int) out.size(0);
-		float[][] transData = new float[nSamples][1];
 
+		double min = 1.0e9D;	// 十分大きな値
+		double v;
 		for (int i = 0; i < nSamples; i++) {
-			double v = out.getDouble(i, 0);
-			transData[i][0] = (float) Math.tanh(v * 0.01e0D);
+			v = out.getDouble(i, 0);
+			if (v < min) {
+				min = v;
+			}
+		}
+		conv = min;
+		
+		float[][] transData = new float[nSamples][1];
+		
+		for (int i = 0; i < nSamples; i++) {
+			double x = out.getDouble(i, 0);
+			double y = x - conv;
+			double z = 1.0e0D / (1.0e0D + Math.exp(-y * 0.01e0D)) - 1.0e0D;
+			
+			transData[i][0] = (float) z;
 		}
 		INDArray transOut = Nd4j.create(transData);
 
 		return transOut;
 
 	}
+	
+	/*
+	 * 
+	 */
+	private INDArray invOutData(INDArray out) {
+		
+		int nSamples = (int) out.size(0);
+		float[][] invData = new float[nSamples][1];
+		
+		for (int i = 0; i < nSamples; i++) {
+			double z = out.getDouble(i, 0);
+			double y = Math.log((1.0e0D / (z + 1.0e0D) - 1.0e0D));
+			double x = conv - y / 0.01e0D;
+			
+			invData[i][0] = (float) x;
+		}
+		INDArray invOut = Nd4j.create(invData);
+
+		return invOut;
+	}
+	
 }
